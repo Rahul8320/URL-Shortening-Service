e24b88d0fa07cf10d09376a21fdcd5fa830e1542
Rahul Dey <rahuldey832000@gmail.com>
2026-01-26 20:54:39 +0530

    Added shorten service

--- src/server/routes/shorten.routes.ts
+++ src/server/routes/shorten.routes.ts
@@ -1,24 +1,7 @@
 import { z } from "zod";
 import { router, publicProcedure } from "../trpc.ts";
-import {
-  createShortenRequest,
-  type Shorten,
-  shortenModel,
-} from "../models/shorten.model.ts";
-import { randomUUID } from "crypto";
-import urlShortenService from "../services/urlShorten.service.ts";
-
-const SHORTENS: Shorten[] = [
-  {
-    id: randomUUID(),
-    url: "https://www.example.com/some/long/url",
-    shortCode: urlShortenService.generateShortCode(
-      "https://www.example.com/some/long/url",
-    ),
-    createdAt: new Date().toISOString(),
-    updatedAt: new Date().toISOString(),
-  },
-];
+import { createShortenRequest, shortenModel } from "../models/shorten.model.ts";
+import ShortenService from "../services/shorten.service.ts";
 
 export const shortenRouter = router({
   createShortenUrl: publicProcedure
@@ -34,15 +17,31 @@ export const shortenRouter = router({
     .input(createShortenRequest)
     .output(shortenModel)
     .mutation(({ input }) => {
-      const id = randomUUID();
-      const shorten: Shorten = {
-        id: id,
-        url: input.url,
-        shortCode: urlShortenService.generateShortCode(input.url),
-        createdAt: new Date().toISOString(),
-        updatedAt: new Date().toISOString(),
-      };
-      SHORTENS.push(shorten);
+      return ShortenService.createShortenUrl(input.url);
+    }),
+  getOriginalUrl: publicProcedure
+    .meta({
+      openapi: {
+        method: "GET",
+        path: "/shorten.getOriginalUrl",
+        tags: ["Shorten"],
+        description: "Get original  url from short code",
+        contentTypes: ["application/json"],
+      },
+    })
+    .input(
+      z.object({
+        shortCode: z.string().describe("Short code of the shorten url"),
+      }),
+    )
+    .output(shortenModel)
+    .query(({ input }) => {
+      const shorten = ShortenService.getShortenByCode(input.shortCode);
+
+      if (shorten === undefined) {
+        throw new Error(`Shorten with code ${input.shortCode} not found`);
+      }
+
       return shorten;
     }),
   getRecentShortenUrls: publicProcedure
@@ -58,6 +57,6 @@ export const shortenRouter = router({
     .input(z.undefined())
     .output(z.array(shortenModel))
     .query(() => {
-      return SHORTENS.toReversed().slice(0, 5);
+      return ShortenService.getRecentShortens();
     }),
 });
